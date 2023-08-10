6d9e35b679569528afcbf78953aae6a10fb7ae51
yomaq <yomaq@bsjm.xyz>
2023-08-09 20:36:42 -0500

    keyboard shortcuts

--- darwinModules/yabai.nix
+++ darwinModules/yabai.nix
@@ -5,19 +5,31 @@
       yabai = {
         enable = true;
         config = {
+          layout = "bsp";
           focus_follows_mouse = "autoraise";
           mouse_follows_focus = "off";
-          mouse_modifier = "fn";
-          window_placement    = "first_child";
+          mouse_modifier = "tab ";
+          window_placement    = "second_child";
           window_opacity      = "on";
           active_window_opacity = 1.0;
           normal_window_opacity = 0.9;
-          top_padding         = 20;
-          bottom_padding      = 10;
-          left_padding        = 10;
-          right_padding       = 10;
-          window_gap          = 10;
+          top_padding         = 5;
+          bottom_padding      = 5;
+          left_padding        = 5;
+          right_padding       = 5;
+          window_gap          = 5;
+          mouse_drop_action = "swap"; 
         };
+      };
+      skhd = {
+        enable = true;
+        skhdConfig = ''
+          shift + cmd - left : yabai -m window --warp west
+          shift + cmd - down : yabai -m window --warp south
+          shift + cmd - up : yabai -m window --warp north
+          shift + cmd - right : yabai -m window --warp east
+          shift + cmd - s : yabai -m window --toggle split
+        '';
       };
     };
   };
