ec99d76ab77bb83659392c24d8b717c43fae2b08
yomaq <yomaq@bsjm.xyz>
2023-08-09 20:54:12 -0500

    change config layout

--- darwinModules/yabai.nix
+++ darwinModules/yabai.nix
@@ -4,31 +4,34 @@
     services = {
       yabai = {
         enable = true;
-        config = {
-          layout = "bsp";
-          focus_follows_mouse = "autoraise";
-          mouse_follows_focus = "off";
-          mouse_modifier = "tab ";
-          window_placement    = "second_child";
-          window_opacity      = "on";
-          active_window_opacity = 1.0;
-          normal_window_opacity = 0.9;
-          top_padding         = 5;
-          bottom_padding      = 5;
-          left_padding        = 5;
-          right_padding       = 5;
-          window_gap          = 5;
-          mouse_drop_action = "swap"; 
-        };
+        extraConfig = ''
+          yabai -m config focus_follows_mouse          on
+          yabai -m config window_placement             second_child
+          yabai -m config window_opacity               on
+          yabai -m config active_window_opacity        1.0
+          yabai -m config normal_window_opacity        0.90
+          yabai -m config auto_balance                 off
+          yabai -m config mouse_modifier               fn
+          yabai -m config mouse_action1                move
+          yabai -m config mouse_action2                resize
+          yabai -m config mouse_drop_action            swap
+  
+          yabai -m config layout                       bsp
+          yabai -m config top_padding                  5
+          yabai -m config bottom_padding               5
+          yabai -m config left_padding                 5
+          yabai -m config right_padding                5
+          #yabai -m rule --add app=""                  manage=off
+        '';
       };
       skhd = {
         enable = true;
         skhdConfig = ''
-          shift + cmd - left : yabai -m window --warp west
-          shift + cmd - down : yabai -m window --warp south
-          shift + cmd - up : yabai -m window --warp north
-          shift + cmd - right : yabai -m window --warp east
-          shift + cmd - s : yabai -m window --toggle split
+          shift + cmd - left    : yabai -m window --warp west
+          shift + cmd - down    : yabai -m window --warp south
+          shift + cmd - up      : yabai -m window --warp north
+          shift + cmd - right   : yabai -m window --warp east
+          shift + cmd - s       : yabai -m window --toggle split
         '';
       };
     };
