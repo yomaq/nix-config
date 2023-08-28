d648b795eb7228dd19e70cb86a12b79e49d28132
yomaq <yomaq@bsjm.xyz>
2023-08-27 21:31:02 -0500

    pause on home entroypoint

--- flake.nix
+++ flake.nix
@@ -65,20 +65,20 @@
 
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        "carln@midnight" = home-manager.lib.homeManagerConfiguration {
-          system = "aarch64-darwin"; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-          # > Our main home-manager configuration file <
-          modules = [ ./home-manager/carlnMidnight.nix ];
-        };
-        "carln@nixos" = home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux"; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-          # > Our main home-manager configuration file <
-          modules = [ ./home-manager/carlnMidnight.nix ];
-        };
-      };
+#      homeConfigurations = {
+#        "carln@midnight" = home-manager.lib.homeManagerConfiguration {
+#          system = "aarch64-darwin"; # Home-manager requires 'pkgs' instance
+#          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+#          # > Our main home-manager configuration file <
+#          modules = [ ./home-manager/carlnMidnight.nix ];
+#        };
+#        "carln@nixos" = home-manager.lib.homeManagerConfiguration {
+#          system = "x86_64-linux"; # Home-manager requires 'pkgs' instance
+#          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+#          # > Our main home-manager configuration file <
+#          modules = [ ./home-manager/carlnMidnight.nix ];
+#        };
+#      };
 
       # Modules for importing without referencing their file location:
       darwinModules = {
