63bd4291c174d3876aa0e7c8da52366d92796b26
yomaq <yomaq@bsjm.xyz>
2023-08-28 03:53:57 -0500

    k

--- home-manager/modules/gnome-home.nix
+++ home-manager/modules/gnome-home.nix
@@ -35,7 +35,6 @@
       "org/gnome/settings-daemon/plugins/media-keys" = {
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
         ];
       };
       # Define the custom options here
