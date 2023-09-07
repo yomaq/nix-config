26a3a1cfee2c74721aaf3f2d47b54e8f5b89369c
yomaq <yomaq@bsjm.xyz>
2023-09-06 21:34:01 -0500

    nixos-anywhere script attempt

---
+++ Utilities/nixos-anywhere/remote-install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo "ipaddress"
+read ipaddress
+echo "hostname in flake"
+read flake
+
+# Create a temporary directory
+temp=$(mktemp -d)
+
+# Function to cleanup temporary directory on exit
+cleanup() {
+  rm -rf "$temp"
+}
+trap cleanup EXIT
+
+# Create the directory where sshd expects to find the host keys
+install -d -m755 "$temp/etc/ssh"
+
+# Decrypt your private key from the password store and copy it to the temporary directory
+op read op://chezmoi/agenix-key/agenix > "$temp/etc/ssh/agenix"
+
+# Set the correct permissions so sshd will accept the key
+chmod 600 "$temp/etc/ssh/agenix"
+
+# Install NixOS to the host system with our secrets and encription
+nix run github:numtide/nixos-anywhere -- --extra-files "$temp" \
+  --disk-encryption-keys /tmp/disk-1.key <(op read op://Private/zfs-encription/password) --flake github:yomaq/nix-config#$flake root@$ipaddress
