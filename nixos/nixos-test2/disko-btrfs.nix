b59e9ff0c138d2433a924431b6d6a9b9e9ddcf16
yomaq <yomaq@bsjm.xyz>
2023-09-10 04:47:04 -0500

    btrfs

--- nixos/nixos-test2/disko-btrfs.nix
+++ nixos/nixos-test2/disko-btrfs.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, modulesPath, inputs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 {
   imports =
     [
@@ -39,9 +39,11 @@
                 extraOpenArgs = [ "--allow-discards" ];
                 # if you want to use the key for interactive login be sure there is no trailing newline
                 # for example use `echo -n "password" > /tmp/secret.key`
-                #passwordFile = "/tmp/secret.key"; # Interactive
                 settings.keyFile = "/tmp/disk-1.key";
-                additionalKeyFiles = [ config.age.secrets.encrypt.path ];
+                ### I tried passing the key in like this:
+                ### settings.keyFile = "/run/agenix/encrypt";
+                ### So that the key would always be loaded in by agenix regardless of Disko does.
+                ### However when disko tries to load at that location it fails as the location doesnt exist.
                 content = {
                   type = "btrfs";
                   extraArgs = [ "-f" ];
