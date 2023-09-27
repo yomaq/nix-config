ee1cba7402304f321b02170bb9c65c48018e845b
yomaq <yomaq@bsjm.xyz>
2023-09-27 03:04:08 -0500

    update

--- nixos/users/carln.nix
+++ nixos/users/carln.nix
@@ -16,7 +16,10 @@
     description = "carln";
     hashedPasswordFile = config.age.secrets.carln.path;
     extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF1TFwXbqdC1UyG75q3HO1n7/L3yxpeRLIq2kQ9DalI"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF1TFwXbqdC1UyG75q3HO1n7/L3yxpeRLIq2kQ9DalI" 
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHDtqEBDvaOC75XewBPGx2F6OszVeKItfro2w1NKW5wy"
+      ];
     packages = with pkgs; [];
   };
 
