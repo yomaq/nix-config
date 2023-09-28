3500131970fc77c6f3eb38db7587d92f420a05f4
yomaq <yomaq@bsjm.xyz>
2023-09-28 02:19:44 -0500

    container updates

--- nixos/modules/containers/pihole.nix
+++ nixos/modules/containers/pihole.nix
@@ -1,16 +1,17 @@
 { config, lib, pkgs, modulesPath, inputs, ... }:
 {
   imports =[
+    # import the container runtime configuration
     ./default.nix
+    # import agenix and impermanance for encryption/ensure the key persists
     inputs.agenix.nixosModules.default
     inputs.impermanence.nixosModules.impermanence
   ];
 
-  # ### agenix secret containing the password in format:
-  # ### WEBPASSWORD: 'set a secure password here or it will be random'
-  # age.secrets.encrypt.file = ../../../secrets/encrypt.age;
-  # # specify which agenix key to use
-  # age.identityPaths = [ "/home/carln/.ssh/agenix" ];
+  ### agenix secrets for container
+  age.secrets.encrypt.file = ../../../secrets/encrypt.age;
+  # specify which agenix key to use
+  age.identityPaths = [ "/home/carln/.ssh/agenix" ];
   # # make sure the key persists between boots
   # environment.persistence."/nix/persistent".files = [
   #     { file = "/etc/nix/containers"; parentDirectory = { mode = "0700"; }; }
@@ -21,10 +22,9 @@
 # it says "tmpfiles" but we don't add rules to remove the tmp file, so its... not tmp?
 # https://discourse.nixos.org/t/creating-directories-and-files-declararively/9349
 # storing volumes in the nix directory because we assume impermanance is wiping root
-# and it makes sense to keep container storage separate from the default persistent folder
   systemd.tmpfiles.rules = [
-    "d /nix/containers/pihole/etc-pihole 0755 root root"
-    "d /nix/containers/pihole//etc-dnsmasq.d 0755 root root"
+    "d /nix/persistent/backup/containers/pihole/etc-pihole 0755 root root"
+    "d /nix/persistent/backup/containers/pihole/etc-dnsmasq.d 0755 root root"
   ];
 
   #networking.firewall.allowedTCPPorts = [80 53];
@@ -37,15 +37,14 @@
       ports = [ "53:53/tcp" "53:53/udp" "80:80/tcp" ];
       environment = {
         "TZ" = "America/Chicago";
-        "WEBPASSWORD" = "test";
+        #"WEBPASSWORD" = "test";
+        "WEBPASSWORD_FILE" = config.age.secrets.encrypt.path
       };
       # environmentFiles = [
-      #   # agenix secret created above
-      #   config.age.secrets.encrypt.path
       # ];
       volumes = [
-        "/nix/containers/pihole/etc-pihole:/etc/pihole"
-        "/nix/containers/pihole//etc-dnsmasq.d:/etc/dnsmasq.d"
+        "/nix/persistent/backup/containers/pihole/etc-pihole:/etc/pihole"
+        "/nix/persistent/backup/containers/pihole/etc-dnsmasq.d:/etc/dnsmasq.d"
       ];
       extraOptions = [
         "--pull=newer"
