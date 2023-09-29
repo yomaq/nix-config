e07848c3937dde3379ab4a13aa5b8afb658725da
yomaq <yomaq@bsjm.xyz>
2023-09-28 21:55:59 -0500

    what

--- secrets/agenix.nix
+++ secrets/agenix.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, inputs, ... }:
 {
+  # I have to do this so I can use agenix in multiple modules, because if I import it directly to multiple modules... it breaks
   imports =
     [
       inputs.agenix.nixosModules.default
