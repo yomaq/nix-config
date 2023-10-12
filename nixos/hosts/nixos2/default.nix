806678a38deb788cf6f6c1128414b475e75cd7d9
yomaq <yomaq@bsjm.xyz>
2023-10-11 23:42:06 -0500

    test

--- nixos/hosts/nixos2/default.nix
+++ nixos/hosts/nixos2/default.nix
@@ -23,29 +23,4 @@
       auto-optimise-store = true;
     };
   };
-
-  services.openssh = {
-    enable = true;
-    hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-  };
-  environment.persistence."/persistent" = {
-    hideMounts = true;
-    files = [
-      { file = "/etc/ssh/ssh/ssh_host_ed25519_key"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-      { file = "/etc/ssh/ssh/ssh_host_rsa_key"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-    ];
-  };
-
-
-
 }
