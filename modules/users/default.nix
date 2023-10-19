92e4aa157a3d460d5fdb1e3a467864405a847597
yomaq <yomaq@bsjm.xyz>
2023-10-18 22:35:59 -0500

    test

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -3,7 +3,7 @@
 let
   cfg = config.yomaq.users;
   addPrefix = name: "./" + name;
-  listDirectories = builtins.map addPrefix cfg.users;
+  listDirectories = builtins.map addPrefix config.yomaq.users.users;
 in
 {
   options.yomaq.users.users = lib.mkOption {
