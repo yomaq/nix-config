13b15ab050539599bbb75c8a8be0aab13e667248
yomaq <yomaq@bsjm.xyz>
2023-10-18 21:48:48 -0500

    test

--- hosts/nixos2/nixos2.nix
+++ hosts/nixos2/nixos2.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, inputs, ... }:
 {
   imports =[
-    inputs.self.nixosModules.common
+    #inputs.self.nixosModules.common
     #inputs.self.nixosModules.options
-    inputs.self.shared.common
+    #inputs.self.shared.common
     #inputs.self.shared.options
   ];
   config = {
