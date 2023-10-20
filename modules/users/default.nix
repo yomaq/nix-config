40e461466b78da769b8731c1049a245dd04ebe78
yomaq <yomaq@bsjm.xyz>
2023-10-19 20:56:02 -0500

    test

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -8,7 +8,7 @@ in
 {
   options.yomaq.users.users = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [ admin ];
+      default = [ "admin" ];
       description = "List of usernames";
     };
   imports = listDirectories;
