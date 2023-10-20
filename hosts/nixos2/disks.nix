6e756e23cac39d1ff5099f94a6b44e9348c6b975
yomaq <yomaq@bsjm.xyz>
2023-10-19 23:34:21 -0500

    test

--- hosts/nixos2/disks.nix
+++ hosts/nixos2/disks.nix
@@ -19,19 +19,13 @@
   environment.persistence."/nix/persistent" = {
     hideMounts = true;
     directories = [
-      "/var/log"
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
     ];
     files = [
-      #"/etc/machine-id"
     ];
-    #users.example = {
-    #  directories = [];
-    #  files = [];
-    #};
   };
 
 
