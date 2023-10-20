67b2d86dca6b2315db00580b402b061b98d980e7
yomaq <yomaq@bsjm.xyz>
2023-10-19 22:23:08 -0500

    test

--- hosts/nixos2/nixos2.nix
+++ hosts/nixos2/nixos2.nix
@@ -2,15 +2,14 @@
 {
   imports =[
     inputs.self.nixosModules.common
-    #inputs.self.nixosModules.options
-    #inputs.self.shared.common
-    #inputs.self.shared.options
+    inputs.self.nixosModules.options
+    inputs.self.shared.common
+    inputs.self.shared.options
     (inputs.self + /users/admin)
   ];
   config = {
     networking.hostName = "nixos";
     system.stateVersion = "23.05";
     networking.useDHCP = lib.mkDefault true;
-    #yomaq.users.users = [ "admin" ];
   };
 }
