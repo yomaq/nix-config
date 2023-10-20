f48bed77e5b2050f576bdcf3973658022aa6b230
yomaq <yomaq@bsjm.xyz>
2023-10-20 00:16:34 -0500

    test

--- modules/nixos/options/ssh/default.nix
+++ modules/nixos/options/ssh/default.nix
@@ -24,17 +24,17 @@ in
         # Disable password ssh authentication
         PasswordAuthentication = false;
       };
-      hostKeys = [
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-        {
-          path = "/etc/ssh/ssh_host_rsa_key";
-          type = "rsa";
-          bits = 4096;
-        }
-      ];
+      # hostKeys = [
+      #   {
+      #     path = "/etc/ssh/ssh_host_ed25519_key";
+      #     type = "ed25519";
+      #   }
+      #   {
+      #     path = "/etc/ssh/ssh_host_rsa_key";
+      #     type = "rsa";
+      #     bits = 4096;
+      #   }
+      # ];
     };
     environment.persistence."/persistent" = {
       hideMounts = true;
