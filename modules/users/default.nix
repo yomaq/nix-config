f76e8f1f9c2f5c65ce4a2ebb2afcd4aab33ae112
yomaq <yomaq@bsjm.xyz>
2023-10-19 20:58:02 -0500

    test

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,13 +1,13 @@
 { options, config, lib, pkgs, ... }:
 
-let
-  addPrefix = name: "./" + name;
-in
+# let
+#   addPrefix = name: "./" + name;
+# in
 {
   options.yomaq.users.users = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [ "admin" ];
       description = "List of usernames";
     };
-  imports = builtins.map addPrefix config.yomaq.users.users;
+  imports = "./" + config.yomaq.users.users;
 }
