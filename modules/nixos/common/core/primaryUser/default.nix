edccdd808910e48bd8c3ed0fa835a552c8349572
yomaq <yomaq@bsjm.xyz>
2023-10-24 00:22:34 -0500

    tailscale module

--- modules/nixos/common/core/primaryUser/default.nix
+++ modules/nixos/common/core/primaryUser/default.nix
@@ -1,7 +1,8 @@
 { options, config, lib, pkgs, ... }:
 
 
-# this is used in other places to allow modules to be dynamic to the primary user accounts on the machine
+# this is used in other places to allow modules to be dynamic to the primary user accounts on the machine, is used frequently in other modules
+# may eventually expand to configure everything related to user accounts
 with lib;
 {
   options.yomaq.primaryUser = {
