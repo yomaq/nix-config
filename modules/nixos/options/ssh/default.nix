51d41a704c4769bd7555dcb2ffd44a88d3c1f032
yomaq <yomaq@bsjm.xyz>
2023-10-23 22:08:51 -0500

    new test

--- modules/nixos/options/ssh/default.nix
+++ modules/nixos/options/ssh/default.nix
@@ -26,11 +26,11 @@ in
       };
       hostKeys = [
         {
-          path = "nix/persistent/etc/ssh/ssh_host_ed25519_key";
+          path = "/etc/ssh/ssh_host_ed25519_key";
           type = "ed25519";
         }
         {
-          path = "nix/persistent/etc/ssh/ssh_host_rsa_key";
+          path = "/etc/ssh/ssh_host_rsa_key";
           type = "rsa";
           bits = 4096;
         }
