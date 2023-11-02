241ff56a8ad8f08b15a7d6ddfaac00e63406a5be
yomaq <yomaq@bsjm.xyz>
2023-11-01 21:25:09 -0500

    test

--- modules/nixos/options/autoUpgradeNixOS/default.nix
+++ modules/nixos/options/autoUpgradeNixOS/default.nix
@@ -19,26 +19,26 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    system.autoUpgrade = {
-      enable = isClean;
-      dates = "hourly";
-      flags = [
-        "--refresh"
-      ];
-      flake = github:yomaq/nix-config;
-    };
+#   config = mkIf cfg.enable {
+#     system.autoUpgrade = {
+#       enable = isClean;
+#       dates = "hourly";
+#       flags = [
+#         "--refresh"
+#       ];
+#       flake = github:yomaq/nix-config;
+#     };
 
-    # Only run if current config (self) is older than the new one.
-    systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
-      serviceConfig.ExecCondition = lib.getExe (
-        pkgs.writeShellScriptBin "check-date" ''
-          lastModified() {
-            nix flake metadata "$1" --refresh --json | ${lib.getExe pkgs.jq} '.lastModified'
-          }
-          test "$(lastModified "${config.system.autoUpgrade.flake}")"  -gt "$(lastModified "self")"
-        ''
-      );
-    };
-  };
-}
+#     # Only run if current config (self) is older than the new one.
+#     systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
+#       serviceConfig.ExecCondition = lib.getExe (
+#         pkgs.writeShellScriptBin "check-date" ''
+#           lastModified() {
+#             nix flake metadata "$1" --refresh --json | ${lib.getExe pkgs.jq} '.lastModified'
+#           }
+#           test "$(lastModified "${config.system.autoUpgrade.flake}")"  -gt "$(lastModified "self")"
+#         ''
+#       );
+#     };
+#   };
+# }
