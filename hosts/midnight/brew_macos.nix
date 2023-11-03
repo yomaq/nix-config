793363f472250d3cf4d18838ad78b0b7c0bd5809
yomaq <yomaq@bsjm.xyz>
2023-11-02 21:22:13 -0500

    prep midnight

---
+++ hosts/midnight/brew_macos.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }: {
+  config = {
+
+#Some programs don't have nix packages available, so making use of Homebrew is needed, sadly there is also no way of installing home brew through nix
+    homebrew = {
+      casks = [
+        "moonlight"
+        "raycast"
+        "arc"
+        "linearmouse"
+        "altserver"
+        "spotify"
+      ];
+    };
+#User specific settings, eventually plan to create the user account itself through Nix as well
+    users = {
+      users = {
+        carln = {
+          home = {
+            _type = "override";
+            content = /Users/carln;
+            priority = 50;
+          };
+          name = "carln";
+          shell = pkgs.zsh;
+        };
+      };
+    };
+  };
+}
