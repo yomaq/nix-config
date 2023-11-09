3515b18559ddf3dbdab818b8f539ad8326c33dcc
yomaq <yomaq@bsjm.xyz>
2023-11-08 23:19:45 -0600

    test

--- modules/containers/template.nix
+++ modules/containers/template.nix
@@ -4,7 +4,7 @@ with lib;
 let
   ### Set container name
   NAME = "tailscale";
-  IMAGE = "tailscale/tailscale";
+  IMAGE = "docker.io/tailscale/tailscale";
   cfg = config.yomaq.pods.${NAME};
 in
 {
