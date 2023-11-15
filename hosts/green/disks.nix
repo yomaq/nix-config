6b12351dd4a337cfda81b7ee22063d18bd3d0922
yomaq <yomaq@bsjm.xyz>
2023-11-14 22:10:22 -0600

    zfs test...

--- hosts/green/disks.nix
+++ hosts/green/disks.nix
@@ -52,6 +52,14 @@ in
 
 
 
+
+  #boot.initrd.postDeviceCommands =
+  #      #wipe / and /var on boot
+  #      lib.mkAfter ''
+  #        zfs rollback -r rpool/root@empty
+  #    '';
+
+
   disko.devices = {
     disk = {
       sda = {
@@ -60,11 +68,6 @@ in
         content = {
           type = "gpt";
           partitions = {
-    ### use either boot or ESP
-            #boot = {
-            # size = "1M";
-            # type = "EF02"; 
-            #};
             ESP = {
               label = "EFI";
               name = "ESP";
@@ -79,31 +82,18 @@ in
                 ];
               };
             };
-            luks = {
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            zfs = {
               size = "100%";
               content = {
-                type = "luks";
-                name = "crypted";
-                passwordFile = "/tmp/secret.key"; 
-                settings.allowDiscards = true;
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/etc/ssh" = {
-                      mountpoint = "/etc/ssh";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "8192M";
-                    };
-                  };
-                };
+                type = "zfs";
+                pool = "zroot";
               };
             };
           };
@@ -118,12 +108,84 @@ in
         ];
       };
     };
+
+
+
+    zpool = {
+      zroot = {
+        type = "zpool";
+        #mode = "mirror";
+        rootFsOptions = {
+          canmount = "off";
+          checksum = "edonr";
+          compression = "zstd";
+          dnodesize = "auto";
+          encryption = "aes-256-gcm";
+          # if you want to use the key for interactive login be sure there is no trailing newline
+          # for example use `echo -n "password" > /tmp/secret.key`
+          keylocation = "file:///tmp/secret.key";
+          keyformat = "passphrase";
+          mountpoint = "none";
+          normalization = "formD";
+          relatime = "on";
+          "com.sun:auto-snapshot" = "false";
+        };
+        postCreateHook = ''
+          zfs set keylocation="prompt" $name;
+        '';
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+
+
+        datasets = {
+          # zfs uses cow free space to delete files when the disk is completely filled
+          reserved = {
+            options = {
+              canmount = "off";
+              mountpoint = "none";
+              reservation = "5GiB";
+            };
+            type = "zfs_fs";
+          };
+          home = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/home";
+            options."com.sun:auto-snapshot" = "true";
+            postCreateHook = "zfs snapshot zroot/home@empty";
+          };
+          persist = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/persist";
+            options."com.sun:auto-snapshot" = "true";
+            postCreateHook = "zfs snapshot zroot/persist@empty";
+          };
+          nix = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/nix";
+            options = {
+              atime = "off";
+              canmount = "on";
+              "com.sun:auto-snapshot" = "true";
+            };
+            postCreateHook = "zfs snapshot zroot/nix@empty";
+          };
+          root = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/";
+            postCreateHook = ''
+              zfs snapshot zroot/root@empty
+              zfs snapshot zroot/root@lastboot
+            '';
+          };
+        };
+      };
+    };
   };
 }
-
-
-
-
-
-
 
---
+++ hosts/green/zfs.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, lib, ... }:
+# Other useful settings come from srvos's zfs module
+{
+  config = lib.mkIf config.boot.zfs.enabled {
+    environment.systemPackages = [
+      pkgs.zfs-prune-snapshots
+    ];
+
+    
+    networking.hostId = "83b0a257";
+
+    boot = {
+      # Newest kernels might not be supported by ZFS
+      kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+      # ZFS does not support swapfiles, disable hibernate and set cache max
+      kernelParams = [
+        "nohibernate"
+        "zfs.zfs_arc_max=17179869184"
+      ];
+      supportedFilesystems = [ "vfat" "zfs" ];
+      zfs = {
+        devNodes = "/dev/disk/by-id/";
+        forceImportAll = true;
+        #removeLinuxDRM = pkgs.hostPlatform.isAarch64;
+        requestEncryptionCredentials = true;
+        #zfs.requestEncryptionCredentials = [ "rpool/root" ];
+      };
+    };
+    
+    services.zfs = {
+      autoScrub.enable = true;
+      trim.enable = true;
+    };
+    # Don't let zfs mount the the datasets, because of legacy mounting
+    #systemd.services.zfs-mount.enable = false;
+  };
+
+}
