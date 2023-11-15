9cbf3c98ad68f07b9439bbc4934563becda34094
yomaq <yomaq@bsjm.xyz>
2023-11-14 20:06:32 -0600

    na

--- hosts/green/hardware-configuration.nix
+++ hosts/green/hardware-configuration.nix
@@ -9,9 +9,5 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
