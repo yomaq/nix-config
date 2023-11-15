bd8424060bf3d569852faeb213f9d509c262599b
yomaq <yomaq@bsjm.xyz>
2023-11-14 20:16:31 -0600

    test

--- Utilities/nixos-anywhere/remote-install-encript.sh
+++ Utilities/nixos-anywhere/remote-install-encript.sh
@@ -18,10 +18,11 @@ install -d -m755 "$temp $persistentDir/etc/ssh/"
 
 # Obtain your private key for agenix from the password store and copy it to the temporary directory
 op read op:"//nix/$hostname/private key" > "$temp $persistentDir/etc/ssh/$hostname"
+op read op:"//nix/$hostname/private key" > "$temp/etc/ssh/$hostname"
 
 # Set the correct permissions so sshd will accept the key
 chmod 600 "$temp $persistentDir/etc/ssh/$hostname"
 
 # Install NixOS to the host system with our secrets and encription
-nix run github:numtide/nixos-anywhere -- --build-on-remote --extra-files "$temp" \
+nix run github:numtide/nixos-anywhere -- --build-on-remote --no-reboot --extra-files "$temp" \
   --disk-encryption-keys /tmp/disk-1.key <(op read op://nix/$hostname/encryption) --flake ..#$hostname root@$ipaddress
