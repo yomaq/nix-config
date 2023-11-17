e98ba71916ba328ef06c5843ca9cb03eda4e0491
yomaq <yomaq@bsjm.xyz>
2023-11-17 03:32:48 -0600

    test

--- hosts/green/disks.nix
+++ hosts/green/disks.nix
@@ -27,11 +27,11 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF1TFwXbqdC1UyG75q3HO1n7/L3yxpeRLIq2kQ9DalI" 
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYSJ9ywFRJ747tkhvYWFkx/Y9SkLqv3rb7T1UuXVBWo"
       ];
-    hostKeys = [ "/etc/ssh/test" ];
+    hostKeys = [ "/etc/ssh/${hostName}-initrd" ];
   };
   boot.initrd.secrets = {
-    "/etc/ssh/test" = "/etc/ssh/test";
-    "/etc/ssh/$test.pub" = "/etc/ssh/test.pub";
+    "/etc/ssh/${hostName}-initrd" = "/etc/ssh/${hostName}";
+    "/etc/ssh/${hostName}-initrd.pub" = "/etc/ssh/${hostName}-initrd.pub";
   };
   # boot.initrd.network.postCommands = lib.mkAfter ''/bin/ash'';
   # boot.initrd.systemd.users.root.shell = "/bin/cryptsetup-askpass";
