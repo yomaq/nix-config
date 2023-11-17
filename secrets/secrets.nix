748bab6bf404d7e49c06b4f328b2ff18d9946e76
yomaq <yomaq@bsjm.xyz>
2023-11-17 03:49:41 -0600

    test

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkt8xgN5ZlTyuSBWAhlv0CCxIN6LmzfSMTHTc53rZ6i";
   carln = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEYoH0dcCQP4sFB3Jl3my7tqXdcwvHo0mOdDdB39UFX";
-  green = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+JAQVPpnno4PNYWSoTIbpNTkJ8EZDPobKFv0oL7tpu";
+  green = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDIHB7suLTRA3Ao/KZmcyCe5ojYAQ72EYoCbkdqlROT";
 
   #example for making groups
   #users = [ user1 user2 ];
