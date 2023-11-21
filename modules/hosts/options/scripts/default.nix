89d8fcfc547258226014a34a8b11a607fb6d20b0
yomaq <yomaq@bsjm.xyz>
2023-11-21 05:53:08 -0600

    unlock script working

--- modules/hosts/options/scripts/default.nix
+++ modules/hosts/options/scripts/default.nix
@@ -17,7 +17,7 @@ in
 
  config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      (import (inputs.self + /modules/scripts/initrdunlock.nix) {inherit pkgs;})
+      (import (inputs.self + /modules/scripts/initrdunlock.nix) {inherit pkgs inputs;})
     ];
  };
 }
--- modules/scripts/initrdunlock.nix
+++ modules/scripts/initrdunlock.nix
@@ -1,48 +1,66 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+
 let
-    nixosHostnames = builtins.attrNames inputs.self.nixosConfigurations;
+    hostnamesList = builtins.attrNames inputs.self.nixosConfigurations;
+    hostnamesString = builtins.concatStringsSep " " hostnamesList;
 in
 
 
 pkgs.writeShellScriptBin "initrd-unlock" ''
 
-# Check if any arguments were provided
-if [ $# -eq 0 ]; then
- # If no arguments were provided, use a default hostname
- hostnames=${nixosHostnames}
-else
- # If arguments were provided, use them as the hostnames
- hostnames="$@"
-fi
+if [ "$1" = "--up" ]; then
+    hostnames="${hostnamesString}"
+    # Iterate over each hostname
+    for hostname in $hostnames; do
+    # Ping the host
+    ping -c 1 "$hostname" > /dev/null 2>&1
 
-# Iterate over each hostname
-for hostname in $hostnames; do
- # Ping the host
- ping -c 1 "$hostname-initrd" > /dev/null 2>&1
-
- # Check if the ping was successful
- if [ $? -eq 0 ]; then
-
-    #sign into 1password and get the secret
-    eval $(op signin)
-    password=$(op read op://nix/$hostname/encryption)
-
-    ssh root@$hostname-initrd "$password"
-    echo "unlock sent"
-
-    sleep 5
-
-    ping -c 1 "$hostname-initrd" > /dev/null 2>&1
-
-    # Check if the initrd sshd server has closed
+    # Check if the ping was successful
     if [ $? -eq 0 ]; then
-        echo "Initrd sshd server still open, unlock may have failed."
+        echo "$hostname is up"
     else
-        echo "Successfully unlocked"
+        echo "Could not reach $hostname"
     fi
- else
-  echo "Could not reach $hostname-initrd"
- fi
-done
+    done
+else
 
+    # Check if any arguments were provided
+    if [ $# -eq 0 ]; then
+    # If no arguments were provided, use all nixos hosts
+    hostnames="${hostnamesString}"
+    else
+    # If arguments were provided, use them as the hostnames
+    hostnames="$@"
+    fi
+
+    # Iterate over each hostname
+    for hostname in $hostnames; do
+        # Ping the host
+        ping -c 1 "$hostname-initrd" > /dev/null 2>&1
+
+        # Check if the ping was successful
+        if [ $? -eq 0 ]; then
+
+            #sign into 1password and get the secret
+            eval $(op signin)
+            password=$(op read op://nix/$hostname/encryption)
+
+            echo -n "$password" | ssh -T root@$hostname-initrd > /dev/null
+            echo "unlock sent"
+
+            sleep 8
+
+            ping -c 1 "$hostname-initrd" > /dev/null 2>&1
+
+            # Check if the initrd sshd server has closed
+            if [ $? -eq 0 ]; then
+                echo "Initrd sshd server still open, unlock may have failed."
+            else
+                echo "Successfully unlocked"
+            fi
+        else
+            echo "Could not reach $hostname-initrd"
+        fi
+    done
+fi
 ''
