e1fa0660adc83380f9bf75ab657892352f15235e
yomaq <yomaq@bsjm.xyz>
2023-11-20 22:37:05 -0600

    remove unneeded files

--- nixos/modules/common/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, modulesPath, inputs, ... }:
-
-{
-  imports =
-    [
-       ./nix+nixos-config.nix
-    ];
-}
--- nixos/modules/impermanence/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, inputs, ... }:
-{
-  # I have to do this so I can import it into multiple modules, because if I import it directly to multiple modules... it breaks
-  imports =
-    [
-      inputs.impermanence.nixosModules.impermanence
-    ];
-}
