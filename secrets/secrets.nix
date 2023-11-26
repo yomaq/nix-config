ba250d5007e73cf7ad28669370471f3fb4c27f4a
yomaq <yomaq@bsjm.xyz>
2023-11-25 22:59:06 -0600

    re-key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,17 +2,19 @@ let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkt8xgN5ZlTyuSBWAhlv0CCxIN6LmzfSMTHTc53rZ6i";
   carln = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEYoH0dcCQP4sFB3Jl3my7tqXdcwvHo0mOdDdB39UFX";
   green = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDIHB7suLTRA3Ao/KZmcyCe5ojYAQ72EYoCbkdqlROT";
+  blue = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/3xL59H/gxyf/zNwM9d0KlovD3GfSGHKgCbmloiGCR";
 
   #example for making groups
   #users = [ user1 user2 ];
+  all = [ agenix carln blue ];
 
 in
 {
-  "carln.age".publicKeys = [ agenix carln green ];
-  "encrypt.age".publicKeys = [ agenix carln ];
-  "tailscaleKey.age".publicKeys = [ agenix carln green ];
-  "tailscaleEnvFile.age".publicKeys = [ agenix green ];
-  "piholeEnvFile.age".publicKeys = [ agenix green ];
+  "carln.age".publicKeys = [ green ] ++ all;
+  "encrypt.age".publicKeys = all;
+  "tailscaleKey.age".publicKeys = [ green ] ++ all;
+  "tailscaleEnvFile.age".publicKeys = [ green ] ++ all;
+  "piholeEnvFile.age".publicKeys = [ green ] ++ all;
 
   #example for calling groups
   #"secret2.age".publicKeys = users ++ systems;
