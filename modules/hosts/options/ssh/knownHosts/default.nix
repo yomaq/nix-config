2d88f011cbe97342562b3a274ab19a3a17c1aea1
yomaq <yomaq@bsjm.xyz>
2023-11-26 00:02:05 -0600

    host keys

--- modules/hosts/options/ssh/knownHosts/default.nix
+++ modules/hosts/options/ssh/knownHosts/default.nix
@@ -3,5 +3,8 @@
   programs.ssh.knownHosts = {
     "green".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWw4+s+Og4ASHmpP5s03O+mww5y1aPa9fE1rZHP1KDD";
     "green-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXJChTqiVIusv+GZ65vK8Uq9f4e4UDgaD3b2AEH6xh1";
+
+    "blue".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhFFEFJ3EhgPqvwG6cFBIRJzNmE2c/owqJ0afPGOplt";
+    "blue-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOknfDLAOkOmZUHByJQEWzwdOJbj+SKoOTGEP+Es0glS";
     };
 }
