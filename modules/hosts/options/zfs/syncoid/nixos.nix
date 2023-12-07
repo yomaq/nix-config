edf1d87497147a2aa55296ec0d091e1e23a38abd
yomaq <yomaq@bsjm.xyz>
2023-12-07 13:28:11 -0600

    test

--- modules/hosts/options/zfs/syncoid/nixos.nix
+++ modules/hosts/options/zfs/syncoid/nixos.nix
@@ -58,32 +58,29 @@ in
     };
   };
 
-  config = ( mkIf config.yomaq.sanoid.enable {
-    # enable syncoid by default on all systems
-    services.syncoid.enable = true;
-  # backup all nixos hosts that are not the backup server or the excluded hosts
-  }) // (mkMerge mappedConfig) 
-  
-  
-  // (mkIf config.yomaq.syncoid.isBackupServer {
-    services.syncoid = {
-      enable = true;
-      interval = "daily";
-      commands."${thisHost}Save" = {
-        source = "zpool/persistSave";
-        target = "zstorage/backups/${thisHost}Save";
-        recvOptions = "c";
+  config = mkMerge [
+    (mkMerge mappedConfig)
+    (mkIf config.yomaq.sandoid.enable {services.syncoid.enable = true;})
+    (mkIf config.yomaq.syncoid.isBackupServer {
+      services.syncoid = {
+        enable = true;
+        interval = "daily";
+        commands."${thisHost}Save" = {
+          source = "zpool/persistSave";
+          target = "zstorage/backups/${thisHost}Save";
+          recvOptions = "c";
+        };
       };
-    };
-    services.sanoid = {
-      datasets."zstorage/backups/${thisHost}Save" = {
-          autosnap = false;
-          autoprune = true;
-          hourly = 0;
-          daily = 14;
-          monthly = 6;
-          yearly = 1;
+      services.sanoid = {
+        datasets."zstorage/backups/${thisHost}Save" = {
+            autosnap = false;
+            autoprune = true;
+            hourly = 0;
+            daily = 14;
+            monthly = 6;
+            yearly = 1;
+        };
       };
-    };
-  });
+    })
+  ]
 }
