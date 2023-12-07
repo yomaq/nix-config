aaac72185f11e5740908bb64955edc0a2ca70b8b
yomaq <yomaq@bsjm.xyz>
2023-12-07 14:51:30 -0600

    test

--- modules/hosts/options/zfs/syncoid/nixos.nix
+++ modules/hosts/options/zfs/syncoid/nixos.nix
@@ -4,34 +4,11 @@ with lib;
 let
   cfg = config.yomaq.syncoid;
   thisHost =  config.networking.hostName;
-  # allNixosHosts = builtins.attrNames inputs.self.nixosConfigurations;
-  allNixosHosts = ["test" "test2" "test3" "test4"];
-  exclude = ["test4" "test3"];
+  allNixosHosts = builtins.attrNames inputs.self.nixosConfigurations;
+  #allNixosHosts = ["test" "test2" "test3" "test4"];
+  exclude = [];
   nixosHosts = lists.subtractLists exclude allNixosHosts;
 
-
-  # configToMap = (mkIf config.yomaq.syncoid.isBackupServer && !builtins.elem hostName cfg.exclude {
-  #   services.syncoid = {
-  #     commands = {
-  #       "${hostName}Save" = {
-  #       source = "syncoid@${hostName}:zpool/persistSave";
-  #       target = "zstorage/backups/${hostName}Save";
-  #       recvOptions = "c";
-  #       };
-  #     };
-  #   };
-  #   services.sanoid = {
-  #     datasets."zstorage/backups/${hostName}Save" = {
-  #         autosnap = false;
-  #         autoprune = true;
-  #         hourly = 0;
-  #         daily = 14;
-  #         monthly = 6;
-  #         yearly = 1;
-  #     };
-  #   };
-  # });
-  # mappedConfig = builtins.map (hostName: configToMap) nixosHosts;
 in
 {
   options.yomaq.syncoid = {
