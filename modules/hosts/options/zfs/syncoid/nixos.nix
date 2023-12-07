8bfcf00bf27a597ec857c09063f5fe9b1f5bbaa2
yomaq <yomaq@bsjm.xyz>
2023-12-07 14:32:04 -0600

    wild test

--- modules/hosts/options/zfs/syncoid/nixos.nix
+++ modules/hosts/options/zfs/syncoid/nixos.nix
@@ -4,8 +4,10 @@ with lib;
 let
   cfg = config.yomaq.syncoid;
   thisHost =  config.networking.hostName;
-  # nixosHosts = builtins.attrNames inputs.self.nixosConfigurations;
-  nixosHosts = ["test" "test2"];
+  # allNixosHosts = builtins.attrNames inputs.self.nixosConfigurations;
+  allNixosHosts = ["test" "test2" "test3" "test4"];
+  exclude = ["test4" "test3"];
+  nixosHosts = lists.subraction exclude allNixosHosts;
 
 
   configToMap = (mkIf config.yomaq.syncoid.isBackupServer && !builtins.elem hostName cfg.exclude {
@@ -47,16 +49,8 @@ in
         will run syncoid and backup other nixos hosts
       '';
     };
-    exclude = mkOption {
-      type = types.listOf types.str;
-      default = [ "${thisHost}" ];
-      description = ''
-        exclude these hosts from being backed up
-      '';
-    };
   };
-  config = mkMerge [
-    mappedConfig
+  config = (mkMerge [
     (mkIf config.yomaq.sandoid.enable {services.syncoid.enable = true;})
     (mkIf config.yomaq.syncoid.isBackupServer {
       services.syncoid = {
@@ -78,5 +72,26 @@ in
             yearly = 1;
         };
       };
-    })];
+    })]) ++
+    (mkIf config.yomaq.syncoid.isBackupServer mkMerge (map ( hostName: {
+      services.syncoid = {
+        commands = {
+          "${hostName}Save" = {
+          source = "syncoid@${hostName}:zpool/persistSave";
+          target = "zstorage/backups/${hostName}Save";
+          recvOptions = "c";
+          };
+        };
+      };
+      services.sanoid = {
+        datasets."zstorage/backups/${hostName}Save" = {
+            autosnap = false;
+            autoprune = true;
+            hourly = 0;
+            daily = 14;
+            monthly = 6;
+            yearly = 1;
+        };
+      };
+    }))hostNames);
 }
