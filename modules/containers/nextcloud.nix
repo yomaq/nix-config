3f028c55b33d0c0e5deea046f0f878c89f078829
yomaq <yomaq@bsjm.xyz>
2023-12-11 01:00:09 -0600

    test

--- modules/containers/nextcloud.nix
+++ modules/containers/nextcloud.nix
@@ -216,7 +216,7 @@ in
           "--pull=newer"
           # "--network=container:TS${NAME}"
           "--pod=${NAME}-pod"
-          # "--dns=8.8.8.8"
+          "--dns=1.1.1.1"
         ];
       };
     };
