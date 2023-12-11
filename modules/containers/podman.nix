15a378cb05196292cd04beabc9762bc18ef1955b
yomaq <yomaq@bsjm.xyz>
2023-12-11 00:44:58 -0600

    test

--- modules/containers/podman.nix
+++ modules/containers/podman.nix
@@ -69,7 +69,7 @@ in {
         # Create a `docker` alias for podman, to use it as a drop-in replacement
         dockerCompat = true;
         # Required for containers under podman-compose to be able to talk to each other.
-        defaultNetwork.settings.dns_enabled = true;
+        # defaultNetwork.settings.dns_enabled = true;
       };
     };
     environment.persistence."${config.yomaq.impermanence.dontBackup}" = {
