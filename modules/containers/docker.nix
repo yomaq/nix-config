8b274a05f44b16a33d4230202af16c065fcef9f1
yomaq <yomaq@bsjm.xyz>
2023-12-13 23:47:45 -0600

    traefik container working

--- modules/containers/docker.nix
+++ modules/containers/docker.nix
@@ -11,73 +11,22 @@ in {
       type = types.bool;
       default = false;
     };
-    traefik = mkOption {
-      description = "Enable traefik for docker with tailscale tls certificates";
-      type = types.bool;
-      default = false;
-    };
   };
 
 
-  config = mkMerge [
-    # enable and configure docker 
-    (mkIf (cfg.enable) {
-      virtualisation.oci-containers.backend = "docker";
-      virtualisation = {
-        docker = {
-          enable = true;
-          autoPrune.enable = true;
+  config = mkIf (cfg.enable) {
+    virtualisation.oci-containers.backend = "docker";
+    virtualisation = {
+      docker = {
+        enable = true;
+        autoPrune.enable = true;
 
-        };
       };
-      environment.persistence."${config.yomaq.impermanence.dontBackup}" = {
-        directories = [
-          "/var/lib/containers/storage"
-        ];
-      };
-    })
-    # enable and configure traefik
-    (mkIf (cfg.traefik) {
-      # services.traefik = {
-      #   package = inputs.self.packages.x86_64-linux.traefik-test;
-      #   enable =true;
-      #   group = "docker";
-      #   staticConfigOptions = {
-      #     global = {
-      #       checkNewVersion = false;
-      #       sendAnonymousUsage = false;
-      #     };
-      #     entryPoints = {
-      #       http = {
-      #         address = ":80";
-      #         http = {
-      #           redirections = {
-      #             entryPoint = {
-      #               to = "https";
-      #               scheme = "https";
-      #             };
-      #           };
-      #         };
-      #       };
-      #       https = {
-      #         address = ":443";
-      #       };
-      #     };
-      #     providers = {
-      #       docker = {
-      #         endpoint = "unix:///var/run/docker.sock";
-      #         exposedByDefault = false;
-      #       };
-      #     };
-      #     certificatesResolvers.tailscale.tailscale = {};
-      #   };
-      # };
-      # environment.persistence."${config.yomaq.impermanence.dontBackup}" = {
-      #   hideMounts = true;
-      #   directories = [
-      #     "/var/lib/traefik"
-      #   ];
-      # };
-    })
-  ];
+    };
+    environment.persistence."${config.yomaq.impermanence.dontBackup}" = {
+      directories = [
+        "/var/lib/containers/storage"
+      ];
+    };
+  };
 }
--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,6 @@
 { pkgs ? import <nixpkgs> { } }: rec {
 
+  # made to try to use the tailscale tls cert with traefik, appears to properly install traefik, but it still wont work with tailscale. Leaving now for reference for other packages.
   traefik-test = pkgs.callPackage ./traefik { };
 
 }
