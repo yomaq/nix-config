d82f77b8b83237bf2860c367be1ae1493ffbaaee
yomaq <yomaq@bsjm.xyz>
2023-12-17 17:24:23 -0600

    test

--- hosts/carob/carob.nix
+++ hosts/carob/carob.nix
@@ -15,7 +15,8 @@
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-    networking.firewall.checkReversePath = "loose";
+    # networking.firewall.checkReversePath = "loose";
+    networking.firewall.enable = false;
 
     yomaq = {
       tailscale = {
