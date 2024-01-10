c91ac88bf924f0c8d1f2e48c4d5396a838a5196e
yomaq <yomaq@bsjm.xyz>
2024-01-09 18:09:38 -0600

    add inherit

--- modules/containers/nextcloud.nix
+++ modules/containers/nextcloud.nix
@@ -14,6 +14,7 @@ let
   inherit (config.networking) hostName;
   inherit (config.yomaq.impermanence) backup;
   inherit (config.yomaq.impermanence) dontBackup;
+  inherit (config.yomaq.tailscale) tailnetName;
 in
 {
   options.yomaq.pods.${NAME} = {
