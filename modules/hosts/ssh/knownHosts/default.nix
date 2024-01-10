a0ce5cc1b4601211337ac0092da97454c5b9e12c
yomaq <112864332+yomaq@users.noreply.github.com>
2024-01-10 03:19:31 -0600

    update keys

--- modules/hosts/ssh/knownHosts/default.nix
+++ modules/hosts/ssh/knownHosts/default.nix
@@ -7,8 +7,8 @@
     "blue".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKoF3ul1ezP+OnokU6uLIQ6/ztUcboQX7trOw1cHg4H";
     "blue-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHizEipg12TEKUZJjCq5exR/Ydpp6iL6gGHtQ5NCWobM";
 
-    "azure".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyHQ6+BRqbiqsK50g5mwkhDHUbh0KVkA6W32UDqV9OD";
-    "azure-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFww5QBVekBnv+NMzRK6L+MIS8XrE6UMVK1xSUv/cHds";
+    "azure".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPJbDYeM58FlEhsWP861yNWG8gn51xbya7OT6q9gGZL";
+    "azure-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQ1sEoQpAUeNcggp1jUpbo0n+gYa6qioOM0vbmg7AVX";
 
     "teal".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7JriToIhfbruPxV0TJI9SF2nTKINmlsnSoyDdAVVoY";
     "teal-initrd".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBX5raO/z+XWBKjOU4JwGvquTMYSgxcg+tCFU3ok5s6H";
