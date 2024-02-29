42d3de0164eb46777a5746826c5027ca7f4f4cd3
yomaq <yomaq@bsjm.xyz>
2024-02-28 20:14:33 -0600

    Revert changes to luks partition in nixos.nix

--- modules/hosts/zfs/disks/nixos.nix
+++ modules/hosts/zfs/disks/nixos.nix
@@ -231,26 +231,26 @@ in
               content = {
                 type = "gpt";
                 partitions = {
-                  # luks = {
-                  #   size = "100%";
-                  #   content = {
-                  #     type = "luks";
-                  #     name = "${diskname}";
-                  #     settings.allowDiscards = true;
-                  #     passwordFile = "/tmp/secret.key";
-                  #     content = {
-                  #       type = "zfs";
-                  #       pool = "zstorage";
-                  #     };
-                  #   };
-                  # };
-                  zfs = {
+                  luks = {
                     size = "100%";
                     content = {
-                      type = "zfs";
-                      pool = "zstorage";
+                      type = "luks";
+                      name = "${diskname}";
+                      settings.allowDiscards = true;
+                      passwordFile = "/tmp/secret.key";
+                      content = {
+                        type = "zfs";
+                        pool = "zstorage";
+                      };
                     };
                   };
+                  # zfs = {
+                  #   size = "100%";
+                  #   content = {
+                  #     type = "zfs";
+                  #     pool = "zstorage";
+                  #   };
+                  # };
                 };
               };
             };
