415f9a0360ac302e3baa291912b4e5d5aa217d57
yomaq <yomaq@bsjm.xyz>
2024-02-29 22:34:58 -0600

    Add sudo configuration to primaryUser module

--- modules/hosts/primaryUser/nixos.nix
+++ modules/hosts/primaryUser/nixos.nix
@@ -14,4 +14,15 @@ with lib;
       '';
     };
   };
+
+  config = {
+    # pulled from https://github.com/nix-community/srvos/blob/main/nixos/common/sudo.nix
+
+    # Only allow members of the wheel group to execute sudo by setting the executable’s permissions accordingly. This prevents users that are not members of wheel from exploiting vulnerabilities in sudo such as CVE-2021-3156.
+    security.sudo.execWheelOnly = true;
+    # Don't lecture the user. Less mutable state.
+    security.sudo.extraConfig = ''
+      Defaults lecture = never
+    '';
+  };
 }
