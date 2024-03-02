85d61f4188192921eec255d83554336c0356f9fa
yomaq <yomaq@bsjm.xyz>
2024-03-02 03:24:15 -0600

    install-iso working, prep smalt

--- hosts/install-iso/install-iso.nix
+++ hosts/install-iso/install-iso.nix
@@ -11,18 +11,24 @@
 
     services.getty.autologinUser = lib.mkDefault "admin";
 
+    environment.systemPackages = with pkgs; [
+      rsync
+    ];
+    networking.wireless.enable = lib.mkForce false;
     yomaq = {
       tailscale = {
         enable = true;
         extraUpFlags = ["--ssh=true" "--reset=true" ];
         # attempt to write the authkey in clear text into the nix store for the install-iso as it won't have a key to decrypt the secret
         authKeyFile = (pkgs.writeText "tailscaleAuthKey" (builtins.readFile config.age.secrets.tailscaleKeyAcceptSsh.path));
+        preApprovedSshAuthkey = true;
       };
       timezone.central= true;
       suites = {
         basics.enable = true;
         foundation.enable = true;
       };
+      # network.basics = lib.mkDefault false;
     };
   };
 }
---
+++ hosts/smalt/smalt.nix
@@ -0,0 +1,65 @@
+{ config, lib, pkgs, inputs, modulesPath, ... }:
+{
+  imports =[
+    # import custom modules
+    inputs.self.nixosModules.yomaq
+    inputs.self.nixosModules.pods
+    # import users
+    (inputs.self + /users/admin)
+    # hardware
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+  config = {
+    networking.hostName = "teal";
+    system.stateVersion = "23.11";
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    
+    yomaq = {
+      autoUpgrade.enable = true;
+      primaryUser.users = [ "carln" "admin" ];
+      tailscale = {
+        enable = true;
+        extraUpFlags = ["--ssh=true" "--reset=true" "--advertise-exit-node"];
+        useRoutingFeatures = "server";
+      };
+      timezone.central= true;
+      syncoid.enable = true;
+      suites = {
+        basics.enable = true;
+        foundation.enable = true;
+      };
+      docker.enable = true;
+      pods = {
+        # valheim.enable = true;
+        minecraft.enable = true;
+        palworld.enable = true;
+        tailscale = {
+          enable = true;
+          TSargs = "--reset=true --advertise-exit-node";
+        };
+      };
+
+      # disk configuration
+      disks = {
+        enable = true;
+        systemd-boot = true;
+        initrd-ssh = {
+          enable = true;
+          ethernetDrivers = ["e1000e"];
+        };
+        zfs = {
+          enable = true;
+          hostID = "f572ff3a";
+          root = {
+            enable = true;
+            disk1 = "nvme0n1";
+            impermanenceRoot = true;
+          };
+        };
+      };
+    };
+  };
+}
