bca307ef1c1596646081a23cea399634f01cd8cb
yomaq <yomaq@bsjm.xyz>
2024-03-02 04:00:44 -0600

    smalt keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,6 +5,7 @@ let
   blue = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/3xL59H/gxyf/zNwM9d0KlovD3GfSGHKgCbmloiGCR";
   azure = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1N/rg1O+vUNIaOVgTJcyB95fufM2PQPEZRso1OlaSu";
   teal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQQRdx0vygfX8LZFLq2Dg8X3EGYLIB+hL788x7LP+29";
+  smalt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsEPoDzF3MRUY0adefhlXkHoErrLncXrV1GTXbM8Znt";
 
   # keys to work for all secrets
   all = [ agenix carln blue ];
@@ -14,13 +15,13 @@ in
   "carln.age".publicKeys = [ green ] ++ all;
   "ryn.age".publicKeys = [ blue ] ++ all;
   "encrypt.age".publicKeys = all;
-  "tailscaleKey.age".publicKeys = [ green azure teal ] ++ all;
+  "tailscaleKey.age".publicKeys = [ green azure teal smalt ] ++ all;
   "tailscaleKeyAcceptSsh.age".publicKeys = [ ] ++ all;
-  "tailscaleEnvFile.age".publicKeys = [ green azure teal ] ++ all;
+  "tailscaleEnvFile.age".publicKeys = [ green azure teal smalt ] ++ all;
   "piholeEnvFile.age".publicKeys = [ green ] ++ all;
   "nextcloudEnvFile.age".publicKeys = [ azure green ] ++ all;
   "nextcloudDBEnvFile.age".publicKeys = [ azure green ] ++ all;
-  "palworldEnvFile.age".publicKeys = [ teal ] ++ all;
+  "palworldEnvFile.age".publicKeys = [ teal smalt ] ++ all;
 
 
   #example for calling groups
