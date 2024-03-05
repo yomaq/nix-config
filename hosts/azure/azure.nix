5dca107993baed9f46db6d0e3ec4e5e416411131
yomaq <yomaq@bsjm.xyz>
2024-03-05 05:22:02 -0600

    test

--- hosts/azure/azure.nix
+++ hosts/azure/azure.nix
@@ -29,6 +29,7 @@
       docker.enable = true;
       pods = {
         nextcloud.enable = true;
+        mealie.enable = true;
       };
       syncoid = { 
         isBackupServer = true;
---
+++ modules/containers/test-mealie.nix
@@ -0,0 +1,79 @@
+
+{ options, config, lib, pkgs, inputs, ... }:
+
+with lib;
+let
+  ### Set container name and image
+  NAME = "mealie";
+  IMAGE = "docker.io/nextcloud";
+  tailscaleIMAGE = "ghcr.io/mealie-recipes/mealie";
+
+
+  cfg = config.yomaq.pods.${NAME};
+  inherit (config.networking) hostName;
+  inherit (config.yomaq.impermanence) backup;
+  inherit (config.yomaq.impermanence) dontBackup;
+  inherit (config.yomaq.tailscale) tailnetName;
+in
+{
+  options.yomaq.pods.${NAME} = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        enable custom ${NAME} container module
+      '';
+    };
+    agenixSecret = mkOption {
+      type = types.path;
+      default = (inputs.self + /secrets/${NAME}EnvFile.age);
+      description = ''
+        path to agenix secret file
+      '';
+    };
+    volumeLocation = mkOption {
+      type = types.str;
+      default = "${backup}/containers/${NAME}";
+      description = ''
+        path to store container volumes
+      '';
+    };
+    imageVersion = mkOption {
+      type = types.str;
+      default = "latest";
+      description = ''
+        container image version
+      '';
+    };
+  };
+
+
+
+
+  config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+    ];
+    virtualisation.oci-containers.containers = {
+### main container
+      "${NAME}" = {
+        image = "${IMAGE}:${cfg.imageVersion}";
+        autoStart = true;
+        environment = {
+          "ALLOW_SIGNUP" = "true";
+        };
+        environmentFiles = [
+        ];
+        volumes = [
+        ];
+        extraOptions = [
+          "--pull=always"
+          "--network=container:TS${NAME}"
+        ];
+      };
+    };
+    yomaq.pods.tailscaled."TS${NAME}" = {
+      enable = true;
+      TSserve = "http://127.0.0.1:9000";
+    };
+  };
+}
