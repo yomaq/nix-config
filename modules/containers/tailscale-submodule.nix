db5e8dc4f7bfd7e3058df3b1f222526ab47e07ba
yomaq <yomaq@bsjm.xyz>
2024-03-05 00:47:12 -0600

    fix

--- modules/containers/tailscale-submodule.nix
+++ modules/containers/tailscale-submodule.nix
@@ -70,7 +70,7 @@ let
     image = "${IMAGE}:${cfg.imageVersion}";
     autoStart = true;
     hostname = cfg.TShostname;
-    environment = lib.mkMrge ({
+    environment = mkMerge ({
       "TS_HOSTNAME" = cfg.TShostname;
       "TS_STATE_DIR"= "/var/lib/tailscale";
       "TS_EXTRA_ARGS" = cfg.TSargs;
