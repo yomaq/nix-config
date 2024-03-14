7c58d7a8761225420b8f1c4b74241d17e718cab1
yomaq <yomaq@bsjm.xyz>
2024-03-14 01:49:47 -0500

    https for grafana

--- modules/containers/teslamate.nix
+++ modules/containers/teslamate.nix
@@ -218,6 +218,6 @@ in
         ];
       };
     };
-    yomaq.pods.tailscaled."TS${NAME}".TSserve = "http://127.0.0.1:4000";
+    yomaq.pods.tailscaled."TS${NAME}".TSserve = "http://127.0.0.1:3000";
   };
 }
