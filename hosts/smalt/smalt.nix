cd36d944552a80ae9206965f5e1845f0d1c0fa9f
yomaq <yomaq@bsjm.xyz>
2024-04-05 01:20:57 -0500

    re-enable auto updates for smalt

--- hosts/smalt/smalt.nix
+++ hosts/smalt/smalt.nix
@@ -18,7 +18,6 @@
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     
     yomaq = {
-      autoUpgrade.enable = false;
       primaryUser.users = [ "admin" ];
       tailscale = {
         enable = true;
