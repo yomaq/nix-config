cd9136421d928084f7f556394607b108b39d5e84
yomaq <yomaq@bsjm.xyz>
2024-05-22 01:07:00 -0500

    fix networking, and add root password

--- hosts/install-iso/install-iso.nix
+++ hosts/install-iso/install-iso.nix
@@ -1,15 +1,13 @@
 { config, lib, pkgs, inputs, modulesPath, ... }:
 {
   imports =[
-    # import custom modules
-    inputs.self.nixosModules.yomaq
     # import users
-    (inputs.self + /users/admin)
+      inputs.self.nixosModules.yomaq
   ];
   config = {
     networking.hostName = "nixos-install";
 
-    services.getty.autologinUser = lib.mkDefault "admin";
+    users.users.root.initialPassword = "k";
 
     environment.systemPackages = with pkgs; [
       rsync
@@ -18,7 +16,7 @@
     yomaq = {
       tailscale = {
         enable = true;
-        extraUpFlags = ["--ssh=true" "--reset=true" ];
+        extraUpFlags = ["--reset=true" ];
         # attempt to write the authkey in clear text into the nix store for the install-iso as it won't have a key to decrypt the secret
         authKeyFile = (pkgs.writeText "tailscaleAuthKey" (builtins.readFile config.age.secrets.tailscaleOAuthKeyAcceptSsh.path));
         preApprovedSshAuthkey = true;
@@ -26,8 +24,10 @@
       timezone.central= true;
       suites = {
         basics.enable = true;
-        foundation.enable = true;
+        # foundation.enable = true;
       };
+      nixSettings.enable = true;
+      # initrd-tailscale.enable = lib.mkDefault false;
       # network.basics = lib.mkDefault false;
     };
   };
