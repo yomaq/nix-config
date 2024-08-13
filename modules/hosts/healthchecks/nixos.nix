e8e509c5b963a45c0dbfeaca8f79589d91ef5899
yomaq <yomaq@bsjm.xyz>
2024-08-12 19:12:21 -0500

    add pearl to healthchecks

--- modules/hosts/healthchecks/nixos.nix
+++ modules/hosts/healthchecks/nixos.nix
@@ -19,6 +19,7 @@ in
         teal = "https://azure-healthchecks.sable-chimaera.ts.net/ping/54c781e4-7f67-42ec-89de-16f98ba55d9f";
         carob = "https://azure-healthchecks.sable-chimaera.ts.net/ping/73e0cf2b-5ccc-40cc-8f9e-badbe053e5b4";
         azure = "https://azure-healthchecks.sable-chimaera.ts.net/ping/53d019ba-c0f5-4354-8335-98eca466e15d";
+        pearl = "https://azure-healthchecks.sable-chimaera.ts.net/ping/95cad3cc-5209-4aa6-a116-62f2ec80d407";
       };
       nixos-upgrade = {
         blue = "https://azure-healthchecks.sable-chimaera.ts.net/ping/9119da9b-5e5c-4bff-9bd5-981a00f05878";
@@ -26,6 +27,7 @@ in
         azure = "https://azure-healthchecks.sable-chimaera.ts.net/ping/d84050d8-b21f-45e2-b2b1-b009615a441e";
         carob = "https://azure-healthchecks.sable-chimaera.ts.net/ping/9de5edba-a351-4968-a434-7ee00026a37a";
         teal = "https://azure-healthchecks.sable-chimaera.ts.net/ping/a0e00318-05d3-4095-8d8b-0944b8d203e0";
+        pearl = "https://azure-healthchecks.sable-chimaera.ts.net/ping/149f738d-7775-4c6b-933e-50c33288b0e5";
       };
     };
   };
