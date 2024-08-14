8f9066780797e4d8ce9a3be44d36bd650dcf58aa
yomaq <yomaq@bsjm.xyz>
2024-08-13 21:32:24 -0500

    steamline

--- modules/scripts/initrdunlock.nix
+++ modules/scripts/initrdunlock.nix
@@ -48,17 +48,6 @@ pkgs.writeShellScriptBin "initrd-unlock" ''
 
               echo -n "$password" | ssh -T root@$hostname-initrd > /dev/null
               echo "unlock sent"
-
-              sleep 8
-
-              ping -c 1 "$hostname-initrd" > /dev/null 2>&1
-
-              # Check if the initrd sshd server has closed
-              if [ $? -eq 0 ]; then
-                  echo "Initrd sshd server still open, unlock may have failed."
-              else
-                  echo "Successfully unlocked"
-              fi
           else
               echo "Could not reach $hostname-initrd"
           fi
