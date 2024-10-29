e2594a26787782cee4ba13fe288982375b2367e8
yomaq <yomaq@bsjm.xyz>
2024-10-28 20:46:22 -0500

    test wsl cuda docker

--- hosts/wsl/wsl.nix
+++ hosts/wsl/wsl.nix
@@ -10,6 +10,7 @@
   imports = [
     # import custom modules
     inputs.self.nixosModules.yomaq
+    inputs.self.nixosModules.pods
     # import users
     (inputs.self + /users/admin)
     inputs.nixos-wsl.nixosModules.default
@@ -20,6 +21,30 @@
 
     wsl.enable = true;
     wsl.defaultUser = "admin";
+    # wsl.useWindowsDriver = true;
+
+
+    # hardware.nvidia-container-toolkit.enable = true;
+    # virtualisation.docker.enableOnBoot = true;
+
+    # # Also enable OpenGL
+    # hardware.opengl = {
+    #   enable = true;
+    #   driSupport = true;
+    #   driSupport32Bit = true;
+    # };
+
+    # environment.systemPackages = with pkgs; [
+    #   cudaPackages.cudatoolkit
+    #   linuxPackages.nvidia_x11
+    #   cudaPackages.cudnn
+    # ];
+    # # programs.zsh.shellInit = ''
+    # #   export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+    # #   export LD_LIBRARY_PATH=/usr/lib/wsl/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+    # #   export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    # #   export EXTRA_CCFLAGS="-I/usr/include"
+    # # '';
 
     yomaq = {
       tailscale = {
@@ -31,6 +56,10 @@
         useRoutingFeatures = "client";
         authKeyFile = null;
       };
+
+      # docker.enable = true;
+      # pods = {
+      # };
 
       autoUpgrade.enable = true;
       primaryUser.users = [ "admin" ];
