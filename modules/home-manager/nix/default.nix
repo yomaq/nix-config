3027b217fa81d007ab76b6601c98fe2b6d9835b1
yomaq <yomaq@bsjm.xyz>
2025-02-10 23:29:43 -0600

    update home-manager settings

--- modules/home-manager/nix/default.nix
+++ modules/home-manager/nix/default.nix
@@ -25,4 +25,7 @@
     };
   };
   xdg.configFile."nixpkgs/config.nix".text = ''{ allowUnfree = true; }'';
+
+  # set home-manager to replace existing files on activation
+  backupFileExtension = "backup";
 }
