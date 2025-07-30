4173deee8440d1488290715164617ed29d3573b6
yomaq <yomaq@bsjm.xyz>
2025-07-30 00:02:14 -0500

    fix gatus api path

--- modules/hosts/autoUpgradeNix/nixos.nix
+++ modules/hosts/autoUpgradeNix/nixos.nix
@@ -50,14 +50,14 @@ in
       systemd.services.nixos-upgrade-fail = lib.mkIf config.system.autoUpgrade.enable {
         script = ''
           ${lib.getExe pkgs.curl} -X POST \
-                  https://azure-gatus.sable-chimaera.ts.net/api/v1/endpoints/nixos-updates_${hostName}/external\?success\=false\&error\= \
+                  https://azure-gatus.sable-chimaera.ts.net/api/v1/endpoints/updates_${hostName}/external\?success\=false\&error\= \
                   -H 'Authorization: Bearer ${hostName}'
         '';
       };
       systemd.services.nixos-upgrade-success = lib.mkIf config.system.autoUpgrade.enable {
         script = ''
           ${lib.getExe pkgs.curl} -X POST \
-                  https://azure-gatus.sable-chimaera.ts.net/api/v1/endpoints/nixos-updates_${hostName}/external\?success\=true\&error\= \
+                  https://azure-gatus.sable-chimaera.ts.net/api/v1/endpoints/updates_${hostName}/external\?success\=true\&error\= \
                   -H 'Authorization: Bearer ${hostName}'
         '';
       };
@@ -68,7 +68,7 @@ in
         map
           (host: {
             name = "${host}";
-            group = "update";
+            group = "updates";
             token = "${host}";
             alerts = [
               {
