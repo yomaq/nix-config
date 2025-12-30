e6fb9518d9b54753631a2b07be4b3ed81027425f
yomaq <yomaq@bsjm.xyz>
2025-12-30 16:31:08 -0600

    add tsidp link to homepage

---
+++ modules/virtualization/microvms/tsidp/nixos.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
+let
+  vmName = "tsidp";
+  vmDeployedOnHost = builtins.any (
+    host: builtins.elem vmName (config.inventory.hosts.${host}.microvms or [ ])
+  ) (builtins.attrNames config.inventory.hosts);
+in
+{
+  config = lib.mkIf (config.yomaq.homepage.enable && vmDeployedOnHost) {
+    yomaq.homepage.groups.services.${vmName} = {
+      icon = "mdi-login-variant";
+      href = "https://${vmName}.${config.yomaq.tailscale.tailnetName}.ts.net/";
+      siteMonitor = "https://${vmName}.${config.yomaq.tailscale.tailnetName}.ts.net/";
+    };
+  };
+}
