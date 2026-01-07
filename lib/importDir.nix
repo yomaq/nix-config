fc6ad3a205defe0764f33e90006dfbe1e0b5e11f
yomaq <yomaq@bsjm.xyz>
2026-01-06 21:48:18 -0600

    nixfmt and general upkeep

--- lib/importDir.nix
+++ lib/importDir.nix
@@ -8,24 +8,31 @@
 #   importDir { dir = ./.; exclude = [ "darwin.nix" ]; excludeExact = [ "default.nix" ]; } # Combine both
 
 dir:
-{ pattern ? ".nix"
-, exclude ? [ ]
-, excludeExact ? [ ]
+{
+  pattern ? ".nix",
+  exclude ? [ ],
+  excludeExact ? [ ],
 }:
 
 let
-  inherit (lib) filter hasSuffix hasPrefix removePrefix;
+  inherit (lib)
+    filter
+    hasSuffix
+    hasPrefix
+    removePrefix
+    ;
 
   allFiles = lib.filesystem.listFilesRecursive dir;
   dirStr = toString dir;
   toRelative = file: removePrefix "${dirStr}/" (toString file);
 in
-filter
-  (file:
-    let relPath = toRelative file;
-    in hasSuffix pattern relPath
-       && relPath != "default.nix"
-       && !(lib.any (pat: hasPrefix pat relPath || hasSuffix pat relPath) exclude)
-       && !(lib.elem relPath excludeExact)
-  )
-  allFiles
+filter (
+  file:
+  let
+    relPath = toRelative file;
+  in
+  hasSuffix pattern relPath
+  && relPath != "default.nix"
+  && !(lib.any (pat: hasPrefix pat relPath || hasSuffix pat relPath) exclude)
+  && !(lib.elem relPath excludeExact)
+) allFiles
--- modules/hosts/zfs/sanoid/nixos.nix
+++ modules/hosts/zfs/sanoid/nixos.nix
@@ -10,7 +10,7 @@ in
   options.yomaq.sanoid = {
     enable = lib.mkOption {
       type = lib.types.bool;
-      default = true;
+      default = false;
       description = ''
         enable custom sanoid, zfs-snapshot module
       '';
