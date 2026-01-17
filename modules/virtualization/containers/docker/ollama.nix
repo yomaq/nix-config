2a08d5aad6032d600a56ebe892a9a4953657e30f
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-01-17 00:03:14 +0000

    Update docker.io/ollama/ollama:latest Docker digest to 4489353

--- modules/virtualization/containers/docker/ollama.nix
+++ modules/virtualization/containers/docker/ollama.nix
@@ -47,7 +47,7 @@ in
 
       virtualisation.oci-containers.containers = {
         "${NAME}" = {
-          image = "docker.io/ollama/ollama:latest@sha256:2c9595c555fd70a28363489ac03bd5bf9e7c5bdf2890373c3a830ffd7252ce6d";
+          image = "docker.io/ollama/ollama:latest@sha256:44893537fcc6f100b70ceb7f5c9fd8f787ba58f9f8ce73bf4a48a5b05fd8c422";
           autoStart = true;
           environment = {
             "OLLAMA_NUM_PARALLEL" = "1";
