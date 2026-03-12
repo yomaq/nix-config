8dfc9e99a5005d8603a179306329e2348069f894
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-03-12 00:11:34 +0000

    Update docker.io/n8nio/n8n:latest Docker digest to 342eeaf

--- modules/virtualization/containers/docker/n8n.nix
+++ modules/virtualization/containers/docker/n8n.nix
@@ -57,7 +57,7 @@ in
 
       virtualisation.oci-containers.containers = {
         "${NAME}" = {
-          image = "docker.io/n8nio/n8n:latest@sha256:b962d7f8ba9e990a0c530256d841fdc52312dce32173f29808e29a9430811ad3";
+          image = "docker.io/n8nio/n8n:latest@sha256:342eeafdb28946fca21d1dd38a1edba6710ecf8b9284021a6779ad8912d3ebf7";
           autoStart = true;
           environment = cfg.env;
           volumes = [ "${cfg.volumeLocation}/n8n_data:/home/node/.n8n" ];
