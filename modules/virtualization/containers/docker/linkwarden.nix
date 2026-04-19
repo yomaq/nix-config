7c98a23e32223c75a36c3e785dd5c318b9712208
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-04-19 00:04:13 +0000

    Update docker.io/postgres:16-alpine Docker digest to 93d5577

--- modules/virtualization/containers/docker/linkwarden.nix
+++ modules/virtualization/containers/docker/linkwarden.nix
@@ -81,7 +81,7 @@ in
       virtualisation.oci-containers.containers = {
         ### DB container
         "DB${NAME}" = {
-          image = "docker.io/postgres:16-alpine@sha256:20edbde7749f822887a1a022ad526fde0a47d6b2be9a8364433605cf65099416";
+          image = "docker.io/postgres:16-alpine@sha256:93d55776e04376e19adb2733e3ccebb4392ee7dd86d8ff238503b30fe719c84f";
           autoStart = true;
           # environment = {
           #     "POSTGRES_PASSWORD" = "password";
