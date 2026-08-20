4fdb13c1875c18f122a5ab69c921989d260199ed
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-08-20 00:12:42 +0000

    Update docker.io/dockurr/windows:latest Docker digest to 9c00d13

--- modules/virtualization/containers/docker/windows-submodule.nix
+++ modules/virtualization/containers/docker/windows-submodule.nix
@@ -50,7 +50,7 @@ let
       };
     };
   mkContainer = name: cfg: {
-    image = "docker.io/dockurr/windows:latest@sha256:3633f055f31aadf76bb650b1ca86897ab45b76ad8eb2cf81e86389ace5eb45ac";
+    image = "docker.io/dockurr/windows:latest@sha256:9c00d13a5db3f671c8b292967e87b3ace813d4a656257c5bc945ccd2afe1558d";
     autoStart = true;
     environment = lib.mkMerge [
       cfg.envVariables
