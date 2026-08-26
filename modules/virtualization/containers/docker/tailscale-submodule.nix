681fa03ed94b6878998278ab822abeaef803fcc4
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-08-26 00:03:37 +0000

    Update ghcr.io/tailscale/tailscale:latest Docker digest to 8c42c45

--- modules/virtualization/containers/docker/tailscale-submodule.nix
+++ modules/virtualization/containers/docker/tailscale-submodule.nix
@@ -94,7 +94,7 @@ let
       };
     in
     {
-      image = "ghcr.io/tailscale/tailscale:latest@sha256:25cde9ad76020b0e29229136d0c38b5962e9a0e1774ffac9b0df68e4a37d6cf0";
+      image = "ghcr.io/tailscale/tailscale:latest@sha256:8c42c4574ab066384fcb72f69e086a2ff1dd3652eb6f56856cee34bcf0d2f680";
       autoStart = true;
       hostname = cfg.TShostname;
       environment = lib.mkMerge [
