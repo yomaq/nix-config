d9440d9c6d4778d810b0b3aeedeb41751c3461ca
yomaq-renovate <yomaq-renovate@bsjm.xyz>
2026-09-26 00:02:18 +0000

    Update ghcr.io/open-webui/open-webui:latest Docker digest to 8b432fe

--- modules/virtualization/containers/docker/ollama-webui.nix
+++ modules/virtualization/containers/docker/ollama-webui.nix
@@ -77,7 +77,7 @@ in
 
       virtualisation.oci-containers.containers = {
         "${NAME}" = {
-          image = "ghcr.io/open-webui/open-webui:latest@sha256:1a6399d237dc392a2313e0ca826020b3fd5d22536357840eb63393d18dc8b924";
+          image = "ghcr.io/open-webui/open-webui:latest@sha256:8b432fe0a65b91116afc7961365c6cca5379cc923171386a96691f3471f3cae9";
           autoStart = true;
           environment = cfg.env;
           environmentFiles = [
